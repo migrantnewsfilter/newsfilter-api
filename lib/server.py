d96d74a2478d9706fc8ea5c558efb0c51f3fce19
Nandan Rao <nandanmarkrao@gmail.com>
2016-11-16 19:27:38 +0100

    supported paging

--- lib/server.py
+++ lib/server.py
@@ -1,5 +1,5 @@
 from __future__ import print_function # In python 2.7
-from flask import Flask
+from flask import Flask, request
 from flask_socketio import SocketIO
 from pymongo import MongoClient, ASCENDING, DESCENDING
 from bson.json_util import dumps
@@ -19,20 +19,30 @@ socketio = SocketIO(app)
 client = MongoClient(
     host = os.environ.get('MONGO_HOST') or None
 )
-collection = client['newsfilter'].news
+
 
 @socketio.on('label')
 def handle_label(data):
+    collection = client['newsfilter'].news
     collection.update_one({ '_id': data['_id']}, {'$set': {'label': data['label']}})
 
-@app.route('/articles')
-def get_articles():
 
+@app.route('/terms')
+def get_sources():
+    collection = client['newsfilter'].terms
+    cursor = collection.find()
+    return dumps(cursor)
+
+
+@app.route('/articles/')
+def get_articles():
+    collection = client['newsfilter'].news
+    start = int(request.args.get('start')) or 0
     # Get top 20, sort by prediction, then by date
     cursor = (collection
-              .find({ 'label': None }, limit=20)
+              .find({ 'label': None })
               .sort([('prediction', -1), ('published', -1)]))
-    return dumps(cursor)
+    return dumps(cursor[start: start+20])
 
 def run():
     socketio.run(app, '0.0.0.0')
